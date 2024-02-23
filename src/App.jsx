0dbe5c3ccf042189cc9e0517af4e112e11adda1a
BLealan <143086275+BLealan@users.noreply.github.com>
2024-02-23 15:40:34 +0000

    added nav

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+import './App.css';
+// Import components
+import Navigation from './components/Navigation';
+
+// Display components via props on page
+function App() {
+  return (
+    <div>
+      <Navigation />
+    </div>
+  );
+}
+export default App
