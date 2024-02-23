14e9bf861b5c2d919895717bc9f43e8685a9dca8
BLealan <143086275+BLealan@users.noreply.github.com>
2024-02-23 16:48:38 +0000

    contact form submission component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { useState } from 'react';
 import './App.css';
 // Import components
-import Header from './components/Header';
-import Navigation from './components/Navigation';
+import Header from './components/HeaderNav/Header';
+import Navigation from './components/HeaderNav/Navigation';
 import Footer from './components/Footer';
 
 // Display components via props on page
