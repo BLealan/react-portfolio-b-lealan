98dccd9d3c8bbb8f936bd8409256de13874f95a0
BLealan <143086275+BLealan@users.noreply.github.com>
2024-02-26 17:46:18 +0000

    added errorpage to route

--- src/components/UI/Footer.jsx
+++ src/components/UI/Footer.jsx
@@ -2,7 +2,7 @@ export default function Footer(){
 
     return (
         <section id="contact">
-        <h3 class="titles">Contact</h3>
+        <h3 className="titles">Contact</h3>
             <ul>
                 <li><a href="mailto:ben_lealan@hotmail.com">This is my Email</a></li>
                 <li><a href="https://github.com/BLealan">This is my GitHub</a></li>
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,48 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
+
 import App from './App.jsx'
-import './index.css'
+import AboutPage from './pages/AboutPage.jsx';
+import ContactPage from './pages/ContactPage.jsx';
+import ErrorPage from './pages/ErrorPage.jsx';
+import HomePage from './pages/HomePage.jsx';
+import PortfolioPage from './pages/PortfolioPage.jsx';
+import ResumePage from './pages/Resume.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: 'about',
+        element: <AboutPage />,
+      },
+      {
+        path: 'contact',
+        element: <ContactPage />,
+      },
+      {
+        path: 'portfolio',
+        element: <PortfolioPage />,
+      },
+      {
+        path: 'resume',
+        element: <ResumePage />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+  <RouterProvider router={router} />
+);
