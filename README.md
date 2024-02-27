a72d4070d6b2f80ad71dbd7b1635530c5084a84f
BLealan <143086275+BLealan@users.noreply.github.com>
2024-02-27 15:29:48 +0000

    portfolio image template

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # React Portfolio
+
+https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_image_overlay_opacity
--- src/pages/PortfolioPage.jsx
+++ src/pages/PortfolioPage.jsx
@@ -1,9 +1,14 @@
+import '../components/UI/portfolioStyle.css'
+
 export default function PortfolioPage() {
     return (
         <div>
-            <p>
-                TEST PORTFOLIO CONTENT
-            </p>
+            <div className="container">
+                <img src="../src/assets/project-recipe.png" alt="recipe-translator" className="image"/>
+                <div className="middle">
+                    <div class="text"><a href='https://blealan.github.io/recipe-translator/'>GitHub</a> / <a href='https://github.com/BLealan/recipe-translator'>Deployed Link</a></div>
+                </div>
+            </div>
         </div>
     );
 }
