7c61a2eb5d0d5faaa26406f655ab68670866fc19
BLealan <143086275+BLealan@users.noreply.github.com>
2024-02-27 15:49:59 +0000

    populate portfolio with links and pictures

--- src/pages/PortfolioPage.jsx
+++ src/pages/PortfolioPage.jsx
@@ -4,9 +4,39 @@ export default function PortfolioPage() {
     return (
         <div>
             <div className="container">
+                <img src="../src/assets/password-generator.png" alt="recipe-translator" className="image"/>
+                <div className="middle">
+                    <div class="text"><a href='https://github.com/BLealan/week-3-password-generator-b-lealan'>GitHub</a> / <a href='https://blealan.github.io/week-3-password-generator-b-lealan/'>Deployed Link</a></div>
+                </div>
+            </div>
+            <div className="container">
+                <img src="../src/assets/day-scheduler.png" alt="recipe-translator" className="image"/>
+                <div className="middle">
+                    <div class="text"><a href='https://github.com/BLealan/work-day-scheduler-b-lealan'>GitHub</a> / <a href='https://blealan.github.io/work-day-scheduler-b-lealan/'>Deployed Link</a></div>
+                </div>
+            </div>
+            <div className="container">
                 <img src="../src/assets/project-recipe.png" alt="recipe-translator" className="image"/>
                 <div className="middle">
                     <div class="text"><a href='https://blealan.github.io/recipe-translator/'>GitHub</a> / <a href='https://github.com/BLealan/recipe-translator'>Deployed Link</a></div>
+                </div>
+            </div>
+            <div className="container">
+                <img src="../src/assets/SVG-logo-generator-demo.gif" alt="recipe-translator" className="image"/>
+                <div className="middle">
+                    <div class="text"><a href='https://github.com/BLealan/SVG-logo-maker-b-lealan'>GitHub</a></div>
+                </div>
+            </div>
+            <div className="container">
+                <img src="../src/assets/travel-blog.png" alt="recipe-translator" className="image"/>
+                <div className="middle">
+                    <div class="text"><a href='https://jbtravelblogfinal-84a20cc94acf.herokuapp.com/'>GitHub</a> / <a href='https://github.com/MustafeMohamoud1/JBTravelBlog'>Deployed Link</a></div>
+                </div>
+            </div>
+            <div className="container">
+                <img src="../src/assets/jate.png" alt="recipe-translator" className="image"/>
+                <div className="middle">
+                    <div class="text"><a href='https://pwa-text-editor-b-lealan.onrender.com/'>GitHub</a> / <a href='https://github.com/BLealan/PWA-text-editor-b-lealan'>Deployed Link</a></div>
                 </div>
             </div>
         </div>
