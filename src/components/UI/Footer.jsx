8ccfac49397f6337b1079553ec7ad1bd810b2302
BLealan <143086275+BLealan@users.noreply.github.com>
2024-02-28 18:07:43 +0000

    added footer icons and links

--- src/components/UI/Footer.jsx
+++ src/components/UI/Footer.jsx
@@ -3,6 +3,21 @@ export default function Footer(){
     return (
         <div className="footer">
             <h6 className="footerText">B Lealan ©2024 👍</h6>
+            <div>
+                <a href="https://github.com/BLealan" target="_blank" rel="noopener noreferrer">
+                <img className="footer-icons" src="../../src/assets/github-logo.svg"/>
+                </a>
+            </div>
+            <div>
+                <a href="https://www.linkedin.com/in/ben-lealan-5a432aa0/" target="_blank" rel="noopener noreferrer">
+                <img className="footer-icons" src="../../src/assets/linkedin-logo.png" href=""/>
+                </a>
+            </div>
+            <div>
+                <a href="https://twitter.com/NotAboutOwls" target="_blank" rel="noopener noreferrer">
+                <img className="footer-icons" src="../../src/assets/twitter-logo.svg.png"/>
+                </a>
+            </div>
         </div>
     )
 }
