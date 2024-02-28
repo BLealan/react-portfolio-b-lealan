d2f63561a86a9062466deb5203a205725ae9497f
BLealan <143086275+BLealan@users.noreply.github.com>
2024-02-28 17:45:45 +0000

    change styling and open portfolio links in new tab

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,64 +1,48 @@
-// import { Link } from 'react-router-dom';
-// import Navigation from './UI/Navigation';
-
-// export default function Nav() {
-//     return (
-//         <Navigation
-//             links={[
-//                 // Link to default Homepage
-//                 <Link key={1} className="" to="/">Home</Link>,
-//                 <Link key={2} className="" to="/about">About Me</Link>,
-//                 <Link key={3} className="" to="/portfolio">Portfolio</Link>,
-//                 <Link key={4} className="" to="/contact">Contact</Link>,
-//                 <Link key={5} className="" to="/resume">Resume</Link>,
-//             ]}
-//         />
-//     );
-// }
-
 import { Link, useLocation } from 'react-router-dom';
 
 function Navigation() {
     const currentPage = useLocation().pathname;
 
     return (
-        <ul className="nav nav-tabs">
-            <li className="nav-item">
-                <Link
-                to="/"
-                className={currentPage === '/' ? 'nav-link active' : 'nav-link'}
-                >Home
-                </Link>
-            </li>
-            <li className="nav-item">
-                <Link
-                to="/About"
-                className={currentPage === '/About' ? 'nav-link active' : 'nav-link'}
-                >About
-                </Link>
-            </li>
-            <li className="nav-item">
-                <Link
-                to="/Contact"
-                className={currentPage === '/Contact' ? 'nav-link active' : 'nav-link'}
-                >Contact
-                </Link>
-            </li>
-            <li className="nav-item">
-                <Link
-                to="/Portfolio"
-                className={currentPage === '/Portfolio' ? 'nav-link active' : 'nav-link'}
-                >Portfolio
-                </Link>
-            </li>
-            <li className="nav-item">
-                <Link
-                to="/Resume"
-                className={currentPage === '/Resume' ? 'nav-link active' : 'nav-link'}
-                >Resume
-                </Link>
-            </li>
-        </ul>
+        <div className='navBar'>
+                <ul className="nav nav-tabs">
+                <li className="nav-item">
+                    <Link
+                    to="/"
+                    className={currentPage === '/' ? 'nav-link active' : 'nav-link'}
+                    >Home
+                    </Link>
+                </li>
+                <li className="nav-item">
+                    <Link
+                    to="/About"
+                    className={currentPage === '/About' ? 'nav-link active' : 'nav-link'}
+                    >About
+                    </Link>
+                </li>
+                <li className="nav-item">
+                    <Link
+                    to="/Contact"
+                    className={currentPage === '/Contact' ? 'nav-link active' : 'nav-link'}
+                    >Contact
+                    </Link>
+                </li>
+                <li className="nav-item">
+                    <Link
+                    to="/Portfolio"
+                    className={currentPage === '/Portfolio' ? 'nav-link active' : 'nav-link'}
+                    >Portfolio
+                    </Link>
+                </li>
+                <li className="nav-item">
+                    <Link
+                    to="/Resume"
+                    className={currentPage === '/Resume' ? 'nav-link active' : 'nav-link'}
+                    >Resume
+                    </Link>
+                </li>
+            </ul>
+        </div>
     );
 }
 
--- src/pages/PortfolioPage.jsx
+++ src/pages/PortfolioPage.jsx
@@ -1,42 +1,43 @@
 import '../components/UI/UIStyle.css'
 
 export default function PortfolioPage() {
+
     return (
         <div>
             <div className="container">
                 <img src="../src/assets/password-generator.png" alt="recipe-translator" className="image"/>
                 <div className="middle">
-                    <div class="text"><a href='https://github.com/BLealan/week-3-password-generator-b-lealan'>GitHub</a> / <a href='https://blealan.github.io/week-3-password-generator-b-lealan/'>Deployed Link</a></div>
+                    <div class="text"><a href='https://github.com/BLealan/week-3-password-generator-b-lealan' target="_blank" rel="noopener noreferrer">GitHub</a> / <a href='https://blealan.github.io/week-3-password-generator-b-lealan/' target="_blank" rel="noopener noreferrer">Deployed Link</a></div>
                 </div>
             </div>
             <div className="container">
                 <img src="../src/assets/day-scheduler.png" alt="recipe-translator" className="image"/>
                 <div className="middle">
-                    <div class="text"><a href='https://github.com/BLealan/work-day-scheduler-b-lealan'>GitHub</a> / <a href='https://blealan.github.io/work-day-scheduler-b-lealan/'>Deployed Link</a></div>
+                    <div class="text"><a href='https://github.com/BLealan/work-day-scheduler-b-lealan' target="_blank" rel="noopener noreferrer">GitHub</a> / <a href='https://blealan.github.io/work-day-scheduler-b-lealan/ target="_blank" rel="noopener noreferrer"'>Deployed Link</a></div>
                 </div>
             </div>
             <div className="container">
                 <img src="../src/assets/project-recipe.png" alt="recipe-translator" className="image"/>
                 <div className="middle">
-                    <div class="text"><a href='https://blealan.github.io/recipe-translator/'>GitHub</a> / <a href='https://github.com/BLealan/recipe-translator'>Deployed Link</a></div>
+                    <div class="text"><a href='https://blealan.github.io/recipe-translator/' target="_blank" rel="noopener noreferrer">GitHub</a> / <a href='https://github.com/BLealan/recipe-translator' target="_blank" rel="noopener noreferrer">Deployed Link</a></div>
                 </div>
             </div>
             <div className="container">
                 <img src="../src/assets/SVG-logo-generator-demo.gif" alt="recipe-translator" className="image"/>
                 <div className="middle">
-                    <div class="text"><a href='https://github.com/BLealan/SVG-logo-maker-b-lealan'>GitHub</a></div>
+                    <div class="text"><a href='https://github.com/BLealan/SVG-logo-maker-b-lealan' target="_blank" rel="noopener noreferrer">GitHub</a></div>
                 </div>
             </div>
             <div className="container">
                 <img src="../src/assets/travel-blog.png" alt="recipe-translator" className="image"/>
                 <div className="middle">
-                    <div class="text"><a href='https://jbtravelblogfinal-84a20cc94acf.herokuapp.com/'>GitHub</a> / <a href='https://github.com/MustafeMohamoud1/JBTravelBlog'>Deployed Link</a></div>
+                    <div class="text"><a href='https://jbtravelblogfinal-84a20cc94acf.herokuapp.com/' target="_blank" rel="noopener noreferrer">GitHub</a> / <a href='https://github.com/MustafeMohamoud1/JBTravelBlog' target="_blank" rel="noopener noreferrer">Deployed Link</a></div>
                 </div>
             </div>
             <div className="container">
                 <img src="../src/assets/jate.png" alt="recipe-translator" className="image"/>
                 <div className="middle">
-                    <div class="text"><a href='https://pwa-text-editor-b-lealan.onrender.com/'>GitHub</a> / <a href='https://github.com/BLealan/PWA-text-editor-b-lealan'>Deployed Link</a></div>
+                    <div class="text"><a href='https://pwa-text-editor-b-lealan.onrender.com/' target="_blank" rel="noopener noreferrer">GitHub</a> / <a href='https://github.com/BLealan/PWA-text-editor-b-lealan' target="_blank" rel="noopener noreferrer">Deployed Link</a></div>
                 </div>
             </div>
         </div>
