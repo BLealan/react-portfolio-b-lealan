0d1838a395cf9e8863456d2f46d7c75e885714b1
BLealan <143086275+BLealan@users.noreply.github.com>
2024-02-29 16:05:30 +0000

    added portrait and content to about

--- README.md
+++ README.md
@@ -15,10 +15,10 @@ WHEN I view the navigation titles
 THEN I am presented with the titles About Me, Portfolio, Contact, and Resume, and the title corresponding to the current section is highlighted
 WHEN I click on a navigation title
 THEN the browser URL changes and I am presented with the corresponding section below the navigation and that title is highlighted -->
-WHEN I load the portfolio the first time
-THEN the About Me title and section are selected by default
-WHEN I am presented with the About Me section
-THEN I see a recent photo or avatar of the developer and a short bio about them
+<!-- WHEN I load the portfolio the first time
+THEN the About Me title and section are selected by default -->
+<!-- WHEN I am presented with the About Me section
+THEN I see a recent photo or avatar of the developer and a short bio about them -->
 <!-- WHEN I am presented with the Portfolio section 
 THEN I see titled images of six of the developer’s applications with links to both the deployed applications and the corresponding GitHub repositories
 WHEN I am presented with the Contact section
--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,9 +1,13 @@
 export default function AboutPage() {
     return (
         <div className="blur-content">
+            <img src="../src/assets/portrait.jpg" className="portrait"/>
             <p className="content-box">
-                Full stack web development! That's what I've been learning to do, and that's what I want to do. Over the past 5 months I've
-                learned how to construct user-focused front end pages, back-end data storage, and the interaction between the two.
+                Full stack web development! That's what I've been learning to do, and that's what I want to do. We all need to start somewhere, so this is it. 
+                My name is Ben and I've been learning to code and programme since September 2023. Have a look at my Portfolio to see some 
+                examples of what I have worked on as well as how to get in touch in Contact 
+                (topics may include but are not limited to boardgames, the work of David Lynch
+                and why 1977 might be the best year for album releases. And also coding!)
             </p>
         </div>
     );
