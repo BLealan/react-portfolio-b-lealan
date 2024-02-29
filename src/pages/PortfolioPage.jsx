15343273839d43b702f8c294e27bd16066eb5296
BLealan <143086275+BLealan@users.noreply.github.com>
2024-02-29 16:39:16 +0000

    add titles to portfolio projects

--- src/pages/PortfolioPage.jsx
+++ src/pages/PortfolioPage.jsx
@@ -5,38 +5,56 @@ export default function PortfolioPage() {
     return (
         <div>
             <div className="container">
-                <img src="../src/assets/password-generator.png" alt="recipe-translator" className="image"/>
+                <img src="../src/assets/password-generator.png" alt="Password generator" className="image"/>
                 <div className="middle">
+                    <div class="text">
+                        <b>Password generator</b>
+                        </div>
                     <div class="text"><a href='https://github.com/BLealan/week-3-password-generator-b-lealan' target="_blank" rel="noopener noreferrer">GitHub</a> / <a href='https://blealan.github.io/week-3-password-generator-b-lealan/' target="_blank" rel="noopener noreferrer">Deployed Link</a></div>
                 </div>
             </div>
             <div className="container">
-                <img src="../src/assets/day-scheduler.png" alt="recipe-translator" className="image"/>
+                <img src="../src/assets/day-scheduler.png" alt="Work Day Scheduler" className="image"/>
                 <div className="middle">
+                <div class="text">
+                        <b>Work Day Scheduler</b>
+                        </div>
                     <div class="text"><a href='https://github.com/BLealan/work-day-scheduler-b-lealan' target="_blank" rel="noopener noreferrer">GitHub</a> / <a href='https://blealan.github.io/work-day-scheduler-b-lealan/ target="_blank" rel="noopener noreferrer"'>Deployed Link</a></div>
                 </div>
             </div>
             <div className="container">
-                <img src="../src/assets/project-recipe.png" alt="recipe-translator" className="image"/>
+                <img src="../src/assets/project-recipe.png" alt="Recipe Translator Group Project" className="image"/>
                 <div className="middle">
+                <div class="text">
+                        <b>Recipe Translator Group Project</b>
+                        </div>
                     <div class="text"><a href='https://blealan.github.io/recipe-translator/' target="_blank" rel="noopener noreferrer">GitHub</a> / <a href='https://github.com/BLealan/recipe-translator' target="_blank" rel="noopener noreferrer">Deployed Link</a></div>
                 </div>
             </div>
             <div className="container">
-                <img src="../src/assets/SVG-logo-generator-demo.gif" alt="recipe-translator" className="image"/>
+                <img src="../src/assets/SVG-logo-generator-demo.gif" alt="SVG Logo Maker" className="image"/>
                 <div className="middle">
+                <div class="text">
+                        <b>SVG Logo Maker</b>
+                        </div>
                     <div class="text"><a href='https://github.com/BLealan/SVG-logo-maker-b-lealan' target="_blank" rel="noopener noreferrer">GitHub</a></div>
                 </div>
             </div>
             <div className="container">
-                <img src="../src/assets/travel-blog.png" alt="recipe-translator" className="image"/>
+                <img src="../src/assets/travel-blog.png" alt="Travel Blog Group Project" className="image"/>
                 <div className="middle">
+                <div class="text">
+                        <b>Travel Blog Group Project</b>
+                        </div>
                     <div class="text"><a href='https://jbtravelblogfinal-84a20cc94acf.herokuapp.com/' target="_blank" rel="noopener noreferrer">GitHub</a> / <a href='https://github.com/MustafeMohamoud1/JBTravelBlog' target="_blank" rel="noopener noreferrer">Deployed Link</a></div>
                 </div>
             </div>
             <div className="container">
-                <img src="../src/assets/jate.png" alt="recipe-translator" className="image"/>
+                <img src="../src/assets/jate.png" alt="PWA Text Editor" className="image"/>
                 <div className="middle">
+                <div class="text">
+                        <b>PWA Text Editor</b>
+                        </div>
                     <div class="text"><a href='https://pwa-text-editor-b-lealan.onrender.com/' target="_blank" rel="noopener noreferrer">GitHub</a> / <a href='https://github.com/BLealan/PWA-text-editor-b-lealan' target="_blank" rel="noopener noreferrer">Deployed Link</a></div>
                 </div>
             </div>
